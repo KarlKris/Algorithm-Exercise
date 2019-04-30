09621d19f24eaea8063bdc6a341e8a1e2a265aed
KarlKris <953591544@qq.com>
2019-04-30 12:27:17 +0800

    build

---
+++ src/Java/test/StringTestDemo.java
@@ -0,0 +1,17 @@
+package Java.test;
+
+/**
+ * @author Li-YuanWen
+ * @title: StringTestDemo
+ * @projectName Algorithm-Exercise
+ * @description: TODO
+ * @date 2019-04-2417:03
+ */
+public class StringTestDemo {
+
+    public static void main(String[] args) {
+        Integer i = null;
+        int res = i;
+        System.out.println(res==i);
+    }
+}
